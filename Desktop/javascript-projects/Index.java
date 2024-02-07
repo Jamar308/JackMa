9fa46040a1e00d4ee40bb958dc74648928056b82
James Goko <jameskaranja308@gmail.com>
2024-02-07 12:57:20 +0300

    version 1

---
+++ Desktop/javascript-projects/Index.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[]args){
+        byte age= 34;
+        long viewscount=3_123_456_789L;
+        Float price=10.99F;
+        Character letter='A';
+        boolean isEligible= false;
+        System.out.println("hello worldS");
+
+    }
+}    
+
+    
+
+
